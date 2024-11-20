b0a60f6f3a9577851873d155d959cca9c6e7ceb2
Yaroslava <ybezuhla@gmail.com>
2024-11-20 11:18:51 +0200

    Fixed a bug by adding headers

--- src/main/java/journal/reading/automation/settings/api/ApiMethods.java
+++ src/main/java/journal/reading/automation/settings/api/ApiMethods.java
@@ -1,13 +1,8 @@
 package journal.reading.automation.settings.api;
 
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
-import org.testng.annotations.Test;
-
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.lessThan;
+
 
 public class ApiMethods {
 
@@ -15,6 +10,7 @@ public class ApiMethods {
         String jsonBody = "{\"title\":\"Депеш моб\",\"author\":\"Сергій Жадан\",\"genre\":\"Роман\",\"coverImage\":\"\",\"top\":true}";
 
         Response response = given().baseUri("http://localhost:5000/api")
+                .header("Content-Type", "application/json")
                 .body(jsonBody)
                 .when().post("/books")
                 .then().assertThat().statusCode(201)
