6c6090f33e957e6fd135fffcae10ec2c73466e66
Yaroslava <ybezuhla@gmail.com>
2025-01-08 16:16:20 +0200

    Changed the method of book transfer and added another check

---
+++ src/main/java/journal/reading/automation/settings/api/JsonUtils.java
@@ -0,0 +1,28 @@
+package journal.reading.automation.settings.api;
+
+import io.qameta.allure.internal.shadowed.jackson.core.type.TypeReference;
+import io.qameta.allure.internal.shadowed.jackson.databind.ObjectMapper;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+
+public class JsonUtils {
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    // Метод для читання JSON із файлу
+    public static String readJsonFromFile(String filePath) throws Exception {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
+
+    // Метод для парсингу JSON до списку об'єктів
+    public static List<Map<String, Object>> parseJsonToList(String json) throws Exception {
+        return mapper.readValue(json, new TypeReference<List<Map<String, Object>>>() {});
+    }
+
+    // Метод для перетворення об'єкта назад у JSON
+    public static String convertObjectToJson(Object object) throws Exception {
+        return mapper.writeValueAsString(object);
+    }
+}
