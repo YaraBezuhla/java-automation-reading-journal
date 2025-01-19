fb1293bb486ef9bd6930442cdf7dfdf413ce21b8
Yaroslava <ybezuhla@gmail.com>
2025-01-19 13:11:04 +0200

    Added test to search by author

--- src/main/java/journal/reading/automation/datas/BookProcessor.java
+++ src/main/java/journal/reading/automation/datas/BookProcessor.java
@@ -1,16 +1,21 @@
 package journal.reading.automation.datas;
 
 import io.qameta.allure.Step;
-
 import java.util.Random;
 
 public class BookProcessor {
 
-    @Step("Метод для отримання рандомного тайтлу")
+    @Step("Метод для отримання рандомної назви книги наявних в enum файлі")
     public String getRandomTitle() {
         BookData[] books = BookData.values();
         int randomIndex = new Random().nextInt(books.length); // Генерація випадкового індексу
         return books[randomIndex].getTitle(); // Повернення тайтлу
     }
 
+    @Step("Метод для отримання рандомного автора книги наявних в enum файлі")
+    public String getRandomAuthor() {
+        BookData[] books = BookData.values();
+        int randomIndex = new Random().nextInt(books.length);
+        return books[randomIndex].getAuthor();
+    }
 }
