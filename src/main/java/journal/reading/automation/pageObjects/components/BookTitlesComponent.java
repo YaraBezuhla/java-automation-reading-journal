e9f85ebc6ce1be8495a9fb857c8238ae98ad8590
Yaroslava <ybezuhla@gmail.com>
2025-04-15 09:46:38 +0300

    Changed BookTitlesComponent

--- src/main/java/journal/reading/automation/pageObjects/components/BookTitlesComponent.java
+++ src/main/java/journal/reading/automation/pageObjects/components/BookTitlesComponent.java
@@ -1,35 +1,34 @@
 package journal.reading.automation.pageObjects.components;
 
 import io.qameta.allure.Step;
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BookTitlesComponent {
 
-    private final WebDriver driver;
-
     public BookTitlesComponent(WebDriver driver) {
-        this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
+    @FindBy(xpath = "//h3[@name]")
+    private List<WebElement> booksName;
 
-    By booksName = By.xpath("//h3[@name]");
-    By loadMoreButtonBooksBlock = By.xpath("//div[@data-test='top-books']//button[@data-test='load-more-button']");
+    @FindBy(xpath = "//div[@data-test='top-books']//button[@data-test='load-more-button']")
+    private WebElement loadMoreButtonBooksBlock;
 
     @Step("Отримати назви книг")
     public ArrayList<String> getBookTitlesOnWebsite() {
         ArrayList<String> titlesList = new ArrayList<>();
-
         while (true) {
             try {
-                WebElement loadMoreButtonWE = driver.findElement(loadMoreButtonBooksBlock);
-                if (loadMoreButtonWE.isDisplayed()) {
-                    loadMoreButtonWE.click();
+                if (loadMoreButtonBooksBlock.isDisplayed()) {
+                    loadMoreButtonBooksBlock.click();
                 } else {
                     break;
                 }
@@ -37,8 +36,7 @@ public class BookTitlesComponent {
                 break;
             }
         }
-        List<WebElement> bookNameArray = driver.findElements(booksName);
-        for (WebElement name : bookNameArray) {
+        for (WebElement name : booksName) {
             String title = name.getText();
             if (!title.isEmpty()) {
                 titlesList.add(title);
