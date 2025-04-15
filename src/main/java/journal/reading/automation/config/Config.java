cf3fc5382ca62432e9ef7721c961232fbc167904
Yaroslava <ybezuhla@gmail.com>
2025-04-15 10:51:23 +0300

    Changed settings

---
+++ src/main/java/journal/reading/automation/config/Config.java
@@ -0,0 +1,11 @@
+package journal.reading.automation.config;
+
+public class Config {
+    public enum BrowserType {
+        CHROME,
+        FIREFOX
+    }
+    public static class Domains {
+        public static final String LOCALE = "http://localhost:8080/";
+    }
+}
--- src/main/java/journal/reading/automation/config/drivers/DriverFactory.java
+++ src/main/java/journal/reading/automation/config/drivers/DriverFactory.java
@@ -1,5 +1,6 @@
 package journal.reading.automation.config.drivers;
 
+import journal.reading.automation.config.Config;
 import journal.reading.automation.config.browserOptions.BrowserOptionsProvider;
 import journal.reading.automation.config.browserOptions.ChromeOptionsProvider;
 import journal.reading.automation.config.browserOptions.FirefoxOptionsProvider;
@@ -10,26 +11,20 @@ import java.util.function.Supplier;
 
 public class DriverFactory {
 
-    public WebDriver CreateWebDriver(BrowserType browserType) {
-        Map<BrowserType, Supplier<Driver>> driverMap = Map.of(
-                BrowserType.Firefox, FirefoxDriverFactory::new,
-                BrowserType.Chrome, ChromeDriverFactory::new
+    public WebDriver CreateWebDriver(Config.BrowserType browserType) {
+        Map<Config.BrowserType, Supplier<Driver>> driverMap = Map.of(
+                Config.BrowserType.FIREFOX, FirefoxDriverFactory::new,
+                Config.BrowserType.CHROME, ChromeDriverFactory::new
         );
 
-        Map<BrowserType, Supplier<BrowserOptionsProvider>> optionsMap = Map.of(
-                BrowserType.Firefox, FirefoxOptionsProvider::new,
-                BrowserType.Chrome, ChromeOptionsProvider::new
+        Map<Config.BrowserType, Supplier<BrowserOptionsProvider>> optionsMap = Map.of(
+                Config.BrowserType.FIREFOX, FirefoxOptionsProvider::new,
+                Config.BrowserType.CHROME, ChromeOptionsProvider::new
         );
 
         Driver driver = driverMap.getOrDefault(browserType, ChromeDriverFactory::new).get();
         BrowserOptionsProvider optionsProvider = optionsMap.getOrDefault(browserType, ChromeOptionsProvider::new).get();
 
         return driver.CreateDriver(optionsProvider.getOptions());
-
-        /*driver = switch (browserType) {
-            case Firefox -> new FirefoxDriverFactory();
-            default -> new ChromeDriverFactory();
-        };
-        return driver.CreateDriver(options);*/
     }
 }
