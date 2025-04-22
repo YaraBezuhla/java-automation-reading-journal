05e46cf36627d7090ce4cf8dde9f57ba8c1a169a
Yaroslava <ybezuhla@gmail.com>
2025-04-22 11:36:45 +0300

    Minimal changed

--- src/main/java/journal/reading/automation/config/utilities/LaunchSettings.java
+++ src/main/java/journal/reading/automation/config/utilities/LaunchSettings.java
@@ -2,14 +2,8 @@ package journal.reading.automation.config.utilities;
 
 import journal.reading.automation.config.drivers.DriverFactory;
 import journal.reading.automation.pageObjects.PageObjectsFacade;
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.testng.annotations.*;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Date;
-
 
 public class LaunchSettings {
 
@@ -32,19 +26,6 @@ public class LaunchSettings {
     public void tearDown() {
         if (driver != null) {
             driver.quit();
-        }
-    }
-
-    public void failedScreenshot(String testMethodName) {
-        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        Date d = new Date();
-
-        String TimeStamp = d.toString().replace(":", "_").replace(" ", "_");
-        try {
-            FileUtils.copyFile(srcFile, new File("C:/Users/Ярослава/Projects/java-automation-reading-journal/build/screenshots/"
-                    + testMethodName + "_" + TimeStamp + ".png"));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 }
